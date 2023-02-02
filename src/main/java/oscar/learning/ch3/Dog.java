75dccc4de6a561457ae5f5cffa281ebbb77b43c0
= <=>
2023-02-02 17:24:02 +0100

    Ch03 - Know your Variables

---
+++ src/main/java/oscar/learning/ch3/Dog.java
@@ -0,0 +1,48 @@
+package oscar.learning.ch3;
+
+public class Dog {
+
+    String name;
+
+    public static void main(String[] args) {
+
+        // make a Dog object and access it
+        Dog dog1 = new Dog();
+        dog1.name = "Bart";
+        System.out.println("Testing dog barks!");
+        dog1.bark();
+        System.out.println();
+
+        // now make a Dog array
+        Dog[] myDogs = new Dog[3];
+        // and put some dogs in it
+        myDogs[0] = new Dog();
+        myDogs[1] = new Dog();
+        myDogs[2] = dog1;
+
+        // now access the Dogs using the array references
+        myDogs[0].name = "Fred";
+        myDogs[1].name = "Marge";
+
+        // now loop through the array and tell all dogs to bark
+        System.out.println("Now let's make all dogs bark!");
+        int x = 0;
+        while (x < myDogs.length) {
+            if (x == 2) {
+                // Hmmmm... what is myDogs[2] name?
+                System.out.print("Last dog’s name is ");
+                System.out.println(myDogs[2].name);
+            }
+            myDogs[x].bark();
+            x++;
+        }
+    }
+    public void bark() {
+        System.out.println(name + " says Ruff!");
+    }
+    public void eat() {
+    }
+    public void chaseCat() {
+    }
+
+}
