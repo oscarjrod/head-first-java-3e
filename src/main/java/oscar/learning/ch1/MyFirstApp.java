e5ad92fab0fcbbaba6a10ae3887625936838b7fe
= <=>
2023-02-02 11:03:09 +0100

    Ch01 - Breaking the Surface

---
+++ src/main/java/oscar/learning/ch1/MyFirstApp.java
@@ -0,0 +1,12 @@
+package oscar.learning.ch1;
+
+public class MyFirstApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("I rule!");
+        System.out.println("The world!");
+
+    }
+
+}
---
+++ src/main/java/oscar/learning/ch1/PhraseOMatic.java
@@ -0,0 +1,35 @@
+package oscar.learning.ch1;
+
+import java.util.Random;
+
+public class PhraseOMatic {
+
+    public static void main(String[] args) {
+
+        String[] wordListOne = {"agnostic", "opinionated", "voice activated", "haptically driven", "extensible",
+                "reactive", "agent based", "functional", "AI enabled", "strongly typed"};
+        String[] wordListTwo = {"loosely coupled", "six sigma", "asynchronous", "event driven", "pub-sub", "IoT",
+                "cloud native", "service oriented", "containerized", "serverless", "microservices", "distributed ledger"};
+        String[] wordListThree = {"framework", "library", "DSL", "REST API", "repository", "pipeline", "service mesh",
+                "architecture", "perspective", "design", "orientation"};
+
+        // find out how many words are in each list
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+
+        // generate three random numbers
+        Random randomGenerator = new Random();
+        int rand1 = randomGenerator.nextInt(oneLength);
+        int rand2 = randomGenerator.nextInt(twoLength);
+        int rand3 = randomGenerator.nextInt(threeLength);
+
+        // now build a phrase
+        String phrase = wordListOne[rand1] + ", " + wordListTwo[rand2] + ", " + wordListThree[rand3].toUpperCase() + "!";
+
+        // print out the phrase
+        System.out.println("What we need is a " + phrase);
+
+    }
+
+}
